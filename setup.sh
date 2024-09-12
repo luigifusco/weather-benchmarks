dce000d703902d6b3dad22eaab5d4c2cc3d68df4
Luigi Fusco <luigifusco98@gmail.com>
2024-09-12 10:51:51 +0200

    handover sync

--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
-export LD_LIBRARY_PATH=/apps/ault/spack/opt/spack/linux-centos8-zen/gcc-8.4.1/cuda-11.7.1-ivzet6dv3q5x2sgmqypy2nge2s34a3ah/lib64/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/apps/ault/spack/opt/spack/linux-centos8-zen/gcc-8.4.1/cuda-11.7.1-ivzet6dv3q5x2sgmqypy2nge2s34a3ah/lib64/:/users/lfusco/spack/opt/spack/linux-sles15-neoverse_v2/gcc-12.3.0/eccodes-2.33.0-f5q67t3r7pymmzsuxet4bhqaaptnhgwi/lib64/:$LD_LIBRARY_PATH
 spack load py-pip
-spack load cuda@11
-spack load nvhpc
-spack load cudnn@8
+spack load hdf5
